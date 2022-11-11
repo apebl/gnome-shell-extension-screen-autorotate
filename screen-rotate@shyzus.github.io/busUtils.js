3de74204d81d8fd56e7d0e5b6cfb091775b407a7
Evan Fosmark <evan.fosmark@gmail.com>
2022-11-10 22:32:19 -0800

    Hotfix: No rotation when mode_props[is-current] is undefined

--- screen-rotate@shyzus.github.io/busUtils.js
+++ screen-rotate@shyzus.github.io/busUtils.js
@@ -36,10 +36,12 @@ var Monitor = class Monitor {
             let mode = modes[i].unpack();
             let id = mode[0].unpack();
             let mode_props = mode[6].unpack();
-            let is_current = mode_props['is-current'].unpack().get_boolean();
-            if (is_current) {
-                this.current_mode_id = id;
-                break;
+            if ('is-current' in mode_props) {
+                let is_current = mode_props['is-current'].unpack().get_boolean();
+                if (is_current) {
+                    this.current_mode_id = id;
+                    break;
+                }
             }
         }
 
