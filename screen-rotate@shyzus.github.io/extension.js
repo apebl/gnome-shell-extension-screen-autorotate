99812f698e3aeb42cd4108e2ae46ddf11091402e
Achmed Waly <12612358+shyzus@users.noreply.github.com>
2023-07-24 19:14:26 +0200

    Update preferences code and add debug mode (#19)
    * Added .gitignore
    
    * Initial prefs rewrite for one switch
    
    * Prefs now work with horizontal/vertical inversion.
    
    * #18 Added flip-orientation preference.
    
    * #18 Added offset spinner in preferences
    
    * #18 Added debug logging in preferences
    
    ---------
    
    Co-authored-by: shyzus <dev@shyzus.com>

--- screen-rotate@shyzus.github.io/extension.js
+++ screen-rotate@shyzus.github.io/extension.js
@@ -1,5 +1,5 @@
 /* extension.js
-* Copyright (C) 2022  kosmospredanie, shyzus, Shinigaminai
+* Copyright (C) 2023  kosmospredanie, shyzus, Shinigaminai
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -35,237 +35,229 @@ const Config = imports.misc.config;
 const [major] = Config.PACKAGE_VERSION.split('.');
 const shellVersion = Number.parseInt(major);
 
+// Orientation names must match those provided by net.hadess.SensorProxy
 const Orientation = Object.freeze({
-    'normal': 0,
-    'left-up': 1,
-    'bottom-up': 2,
-    'right-up': 3
+  'normal': 0,
+  'left-up': 1,
+  'bottom-up': 2,
+  'right-up': 3
 });
 
 var interval = null;
 
 class SensorProxy {
-    constructor(rotate_cb) {
-        this._rotate_cb = rotate_cb;
-        this._proxy = null;
-        this._enabled = false;
-        this._watcher_id = Gio.bus_watch_name(
-            Gio.BusType.SYSTEM,
-            'net.hadess.SensorProxy',
-            Gio.BusNameWatcherFlags.NONE,
-            this.appeared.bind(this),
-            this.vanished.bind(this)
-        );
-    }
+  constructor(rotate_cb) {
+    this._rotate_cb = rotate_cb;
+    this._proxy = null;
+    this._enabled = false;
+    this._watcher_id = Gio.bus_watch_name(
+      Gio.BusType.SYSTEM,
+      'net.hadess.SensorProxy',
+      Gio.BusNameWatcherFlags.NONE,
+      this.appeared.bind(this),
+      this.vanished.bind(this)
+    );
+  }
 
-    destroy() {
-        Gio.bus_unwatch_name(this._watcher_id);
-        if (this._enabled) this.disable();
-        this._proxy = null;
-    }
+  destroy() {
+    Gio.bus_unwatch_name(this._watcher_id);
+    if (this._enabled) this.disable();
+    this._proxy = null;
+  }
 
-    enable() {
-        this._enabled = true;
-        if (this._proxy === null) return;
-        this._proxy.call_sync('ClaimAccelerometer', null, Gio.DBusCallFlags.NONE, -1, null);
-    }
+  enable() {
+    this._enabled = true;
+    if (this._proxy === null) return;
+    this._proxy.call_sync('ClaimAccelerometer', null, Gio.DBusCallFlags.NONE, -1, null);
+  }
 
-    disable() {
-        this._enabled = false;
-        if (this._proxy === null) return;
-        this._proxy.call_sync('ReleaseAccelerometer', null, Gio.DBusCallFlags.NONE, -1, null);
-    }
+  disable() {
+    this._enabled = false;
+    if (this._proxy === null) return;
+    this._proxy.call_sync('ReleaseAccelerometer', null, Gio.DBusCallFlags.NONE, -1, null);
+  }
 
-    appeared(connection, name, name_owner) {
-        this._proxy = Gio.DBusProxy.new_for_bus_sync(
-            Gio.BusType.SYSTEM, Gio.DBusProxyFlags.NONE, null,
-            'net.hadess.SensorProxy', '/net/hadess/SensorProxy', 'net.hadess.SensorProxy',
-            null);
-        this._proxy.connect('g-properties-changed', this.properties_changed.bind(this));
-        if (this._enabled) {
-            this._proxy.call_sync('ClaimAccelerometer', null, Gio.DBusCallFlags.NONE, -1, null);
-        }
+  appeared(connection, name, name_owner) {
+    this._proxy = Gio.DBusProxy.new_for_bus_sync(
+      Gio.BusType.SYSTEM, Gio.DBusProxyFlags.NONE, null,
+      'net.hadess.SensorProxy', '/net/hadess/SensorProxy', 'net.hadess.SensorProxy',
+      null);
+    this._proxy.connect('g-properties-changed', this.properties_changed.bind(this));
+    if (this._enabled) {
+      this._proxy.call_sync('ClaimAccelerometer', null, Gio.DBusCallFlags.NONE, -1, null);
     }
+  }
 
-    vanished(connection, name) {
-        this._proxy = null;
-    }
+  vanished(connection, name) {
+    this._proxy = null;
+  }
 
-    properties_changed(proxy, changed, invalidated) {
-        if (!this._enabled) return;
-        let properties = changed.deep_unpack();
-        for (let [name, value] of Object.entries(properties)) {
-            if (name != 'AccelerometerOrientation') continue;
-            let target = value.unpack();
-            this._rotate_cb(target);
-        }
+  properties_changed(proxy, changed, invalidated) {
+    if (!this._enabled) return;
+    let properties = changed.deep_unpack();
+    for (let [name, value] of Object.entries(properties)) {
+      if (name != 'AccelerometerOrientation') continue;
+      let target = value.unpack();
+      this._rotate_cb(target);
     }
+  }
 }
 
 class ScreenAutorotate {
-    constructor() {
-        this._system_actions = new SystemActions.getDefault();
-        this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.screen-rotate');
-        this._system_actions_backup = null;
-        this._override_system_actions();
-        this._orientation_settings = new Gio.Settings({ schema_id: ORIENTATION_LOCK_SCHEMA });
-        this._orientation_settings.connect('changed::' + ORIENTATION_LOCK_KEY, this._orientation_lock_changed.bind(this));
+  constructor() {
+    this._system_actions = new SystemActions.getDefault();
+    this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.screen-rotate');
+    this._system_actions_backup = null;
+    this._override_system_actions();
+    this._orientation_settings = new Gio.Settings({ schema_id: ORIENTATION_LOCK_SCHEMA });
+    this._orientation_settings.connect('changed::' + ORIENTATION_LOCK_KEY, this._orientation_lock_changed.bind(this));
 
-        this._sensor_proxy = new SensorProxy( this.rotate_to.bind(this) );
+    this._sensor_proxy = new SensorProxy(this.rotate_to.bind(this));
 
-        this._state = false; // enabled or not
+    this._state = false; // enabled or not
 
-        let locked = this._orientation_settings.get_boolean(ORIENTATION_LOCK_KEY);
-        if (!locked) this.enable();
-    }
+    let locked = this._orientation_settings.get_boolean(ORIENTATION_LOCK_KEY);
+    if (!locked) this.enable();
+  }
 
-    _override_system_actions() {
-        this._system_actions_backup = {
-            '_updateOrientationLock': this._system_actions._updateOrientationLock
-        };
+  _override_system_actions() {
+    this._system_actions_backup = {
+      '_updateOrientationLock': this._system_actions._updateOrientationLock
+    };
 
-        this._system_actions._updateOrientationLock = function() {
+    this._system_actions._updateOrientationLock = function() {
 
-            let intervalCounter = 0;
+      let intervalCounter = 0;
 
-            interval = setInterval(() => {
+      interval = setInterval(() => {
 
-                if( this._actions.has(SystemActions.LOCK_ORIENTATION_ACTION_ID)){
-                    try {
-                        this._actions.get(SystemActions.LOCK_ORIENTATION_ACTION_ID).available = true;
-                        this.notify('can-lock-orientation');
-                        clearInterval(interval);
-                    } catch (err) {
-                        logError(err, "Lock Orientation action not initialized.")
-                    }
-                } else if(intervalCounter > 10) {
-                    clearInterval(interval);
-                    logError(new Error("Maximum orientation-lock action interval reached."), "Failed to find orientation-lock action!");
-                }
-                intervalCounter++;
-            }, 1000)
-
-        };
-        
-        this._system_actions._updateOrientationLock();
-    }
-
-    _restore_system_actions() {
-        if (this._system_actions_backup === null) return;
-        this._system_actions._updateOrientationLock = this._system_actions_backup['_updateOrientationLock'];
-        this._system_actions._updateOrientationLock();
-        this._system_actions_backup = null;
-    }
-
-    _orientation_lock_changed() {
-        let locked = this._orientation_settings.get_boolean(ORIENTATION_LOCK_KEY);
-        if (this._state == locked) {
-            this.toggle();
+        if (this._actions.has(SystemActions.LOCK_ORIENTATION_ACTION_ID)) {
+          try {
+            this._actions.get(SystemActions.LOCK_ORIENTATION_ACTION_ID).available = true;
+            this.notify('can-lock-orientation');
+            clearInterval(interval);
+          } catch (err) {
+            logError(err, "Lock Orientation action not initialized.")
+          }
+        } else if (intervalCounter > 10) {
+          clearInterval(interval);
+          logError(new Error("Maximum orientation-lock action interval reached."), "Failed to find orientation-lock action!");
         }
-    }
+        intervalCounter++;
+      }, 1000)
 
-    destroy() {
-        this._sensor_proxy.destroy();
-        this._orientation_settings = null;
-        this._restore_system_actions();
-        clearInterval(interval);
-        interval = null;
-    }
+    };
 
-    toggle() {
-        if (this._state) {
-            this.disable();
-        } else {
-            this.enable();
+    this._system_actions._updateOrientationLock();
+  }
+
+  _restore_system_actions() {
+    if (this._system_actions_backup === null) return;
+    this._system_actions._updateOrientationLock = this._system_actions_backup['_updateOrientationLock'];
+    this._system_actions._updateOrientationLock();
+    this._system_actions_backup = null;
+  }
+
+  _orientation_lock_changed() {
+    let locked = this._orientation_settings.get_boolean(ORIENTATION_LOCK_KEY);
+    if (this._state == locked) {
+      this.toggle();
+    }
+  }
+
+  destroy() {
+    this._sensor_proxy.destroy();
+    this._orientation_settings = null;
+    this._restore_system_actions();
+    clearInterval(interval);
+    interval = null;
+  }
+
+  toggle() {
+    if (this._state) {
+      this.disable();
+    } else {
+      this.enable();
+    }
+  }
+  enable() {
+    this._sensor_proxy.enable();
+    this._state = true;
+  }
+
+  disable() {
+    this._sensor_proxy.disable();
+    this._state = false;
+  }
+
+  rotate_to(orientation) {
+    const sensor = Orientation[orientation];
+    const invert_horizontal_direction = this._settings.get_boolean('invert-horizontal-rotation-direction');
+    const invert_vertical_direction = this._settings.get_boolean('invert-vertical-rotation-direction');
+    const offset = this._settings.get_int('orientation-offset');
+    let target = sensor; // Default to sensor output.
+
+    switch (sensor) {
+      case 0:
+        // sensor reports landscape
+        if (invert_horizontal_direction) {
+          target = 2;
         }
-    }
-    enable() {
-        this._sensor_proxy.enable();
-        this._state = true;
-    }
-
-    disable() {
-        this._sensor_proxy.disable();
-        this._state = false;
-    }
-
-    rotate_to(orientation) {
-        /*
-            Assuming landscape orientation.
-            (sensor starts from 0. +1 is added for math convenience.)
-            Orientation values: 
-            See line 38.
-        */
-
-        let offset = 0;
-        if (this._settings.get_boolean('portrait-display-flipped')) {
-            offset = 1;
+        break;
+      case 1:
+        // sensor reports portrait
+        if (invert_vertical_direction) {
+          target = 3;
         }
-
-        const sensor_output = (Orientation[orientation] + offset) % 4;
-        let sensor = sensor_output + 1; 
-        let target = sensor_output;
-        let reverse_horizontal_direction = this._settings.get_boolean('flip-horizontal-rotation-direction');
-        let reverse_vertical_direction = this._settings.get_boolean('flip-vertical-rotation-direction');
-        let flip_orientation = this._settings.get_boolean('flip-orientation');
-
-        // This means it is horizontal,
-        if (sensor % 2 == 0) {
-
-            if (reverse_horizontal_direction) {
-                if (sensor == 4) {
-                    offset -= 2;
-                } else if(sensor == 2) {
-                    offset += 2;
-                }
-            }
-
-        } else {
-            // It has to be vertical.
-            if (reverse_vertical_direction) {
-                if (sensor == 1) {
-                    offset += 2;
-                } else if(sensor == 3) {
-                    offset -= 2;
-                }
-            }
+        break;
+      case 2:
+        // sensor reports landscape-inverted
+        if (invert_horizontal_direction) {
+          target = 0;
         }
-
-
-        // Default becomes 2
-        if (flip_orientation) {
-            offset += 1
+        break;
+      case 3:
+        // sensor reports portrait-inverted
+        if (invert_vertical_direction) {
+          target = 1;
         }
-
-        target += offset;
-        Rotator.rotate_to(target);
+        break;
     }
+
+    target += offset;
+    if (this._settings.get_boolean('debug-logging')) {
+      log(`sensor=${Orientation[orientation]}`);
+      log(`offset=${offset}`);
+      log(`target=${target}`);
+    }
+    Rotator.rotate_to(target);
+  }
 }
 
 class Extension {
-    constructor(uuid) {
-        this._uuid = uuid;
-        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
-    }
+  constructor(uuid) {
+    this._uuid = uuid;
+    ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+  }
 
-    enable() {
-        this._ext = new ScreenAutorotate();
-    }
+  enable() {
+    this._ext = new ScreenAutorotate();
+  }
 
-    disable() {
-        /*
-            Comment for unlock-dialog usage:
-            The unlock-dialog sesson-mode is usefull for this extension as it allows
-            the user to rotate their screen or lock rotation after their device may
-            have auto-locked. This provides the ability to log back in regardless of 
-            the orientation of the device in tablet mode.
-        */
-        this._ext.destroy();
-        this._ext = null;
-    }
+  disable() {
+    /*
+        Comment for unlock-dialog usage:
+        The unlock-dialog sesson-mode is usefull for this extension as it allows
+        the user to rotate their screen or lock rotation after their device may
+        have auto-locked. This provides the ability to log back in regardless of 
+        the orientation of the device in tablet mode.
+    */
+    this._ext.destroy();
+    this._ext = null;
+  }
 }
 
 function init(meta) {
-    return new Extension(meta.uuid);
+  return new Extension(meta.uuid);
 }
 
--- screen-rotate@shyzus.github.io/prefs.js
+++ screen-rotate@shyzus.github.io/prefs.js
@@ -1,5 +1,5 @@
 /* prefs.js
-* Copyright (C) 2022  kosmospredanie, shyzus, Shinigaminai
+* Copyright (C) 2023  kosmospredanie, shyzus, Shinigaminai
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -15,112 +15,209 @@
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-const GObject = imports.gi.GObject;
-const Gtk = imports.gi.Gtk;
-const Gio = imports.gi.Gio;
+const { GObject, Gtk, Gio, Adw } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-function init () {}
+function init() { }
 
-function buildPrefsWidget () {
-  this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.screen-rotate');
+function fillPreferencesWindow(window) {
+  const settings = ExtensionUtils.getSettings();
 
-    // Create a parent widget that we'll return from this function
-  const prefsWidget = new Gtk.Grid({
-    margin_top: 10,
-    margin_bottom: 10,
-    margin_start: 10,
-    margin_end: 10,
-    column_spacing: 12,
-    row_spacing: 12,
-    visible: true,
+  const page = new Adw.PreferencesPage();
+  window.add(page);
+
+  const orientationGroup = new Adw.PreferencesGroup();
+  orientationGroup.set_title('Orientation Settings')
+  page.add(orientationGroup);
+
+  const debugGroup = new Adw.PreferencesGroup();
+  debugGroup.set_title('Debug Settings');
+  page.add(debugGroup);
+
+  const invertHorizontalRow = new Adw.ActionRow({
+    title: 'Invert horizontal rotation'
+  });
+  orientationGroup.add(invertHorizontalRow);
+
+  const invertVerticalRow = new Adw.ActionRow({
+    title: 'Invert vertical rotation'
+  });
+  orientationGroup.add(invertVerticalRow);
+
+  const flipOrientationRow = new Adw.ActionRow({
+    title: 'Flip orientation',
+    subtitle: 'e.g: Landscape to Portrait. Default is Landscape'
+  });
+  orientationGroup.add(flipOrientationRow);
+
+  const setOffsetRow = new Adw.ActionRow({
+    title: 'Set orientation offset',
+    subtitle: 'Valid offset range: -3 to 3. Default is 0\nExperiment with this in case\
+ orientation is incorrect due to the display being mounted in a non-landscape orientation\
+ e.g PineTab2 or GPD Pocket 3'
   });
 
-  // Add a simple title and add it to the prefsWidget
-  const title = new Gtk.Label({
-    label: `<b>${Me.metadata.name} Preferences</b>`,
-    halign: Gtk.Align.START,
-    use_markup: true,
-    visible: true,
-  });
-  prefsWidget.attach(title, 0, 0, 2, 1);
+  orientationGroup.add(setOffsetRow);
 
-  // Create a label & switch for `flip-rotation-direction-horizontal`
-  const flipRotationHorizontalLabel = new Gtk.Label({
-    label: 'Invert horizontal rotation:',
-    halign: Gtk.Align.START,
-    visible: true,
+  const toggleLoggingRow = new Adw.ActionRow({
+    title: 'Enable debug logging',
+    subtitle: 'Use "journalctl /usr/bin/gnome-shell -f" to see log output.'
   });
-  // Create a label & switch for `flip-rotation-direction-vertical`
-  const flipRotationVerticalLabel = new Gtk.Label({
-    label: 'Invert vertical rotation:',
-    halign: Gtk.Align.START,
-    visible: true,
-  });
-  // Create a label & switch for `flip-orientation`
-  const flipOrientationLabel = new Gtk.Label({
-    label: 'Flip orientation(default=Landscape):',
-    halign: Gtk.Align.START,
-    visible: true,
+  debugGroup.add(toggleLoggingRow);
+
+  const invertHorizontalRotationSwitch = new Gtk.Switch({
+    active: settings.get_boolean('invert-horizontal-rotation-direction'),
+    valign: Gtk.Align.CENTER,
   });
 
-  const portraitDisplayFlipLabel = new Gtk.Label({
-    label: 'Mirrored Portrait Display (ex: GPD Pocket 3):',
-    halign: Gtk.Align.START,
-    visible: true,
+  const invertVerticalRotationSwitch = new Gtk.Switch({
+    active: settings.get_boolean('invert-vertical-rotation-direction'),
+    valign: Gtk.Align.CENTER,
   });
 
-  prefsWidget.attach(flipRotationHorizontalLabel, 0, 1, 1, 1);
-  prefsWidget.attach(flipRotationVerticalLabel, 0, 2, 1, 1);
-  prefsWidget.attach(flipOrientationLabel, 0, 3, 1, 1);
-  prefsWidget.attach(portraitDisplayFlipLabel, 0, 4, 1, 1);
-
-  this.flipHorizontalRotationSwitch = new Gtk.Switch({
-    halign: Gtk.Align.END,
-    visible: true,
+  const flipOrientationSwitch = new Gtk.Switch({
+    active: settings.get_boolean('flip-orientation'),
+    valign: Gtk.Align.CENTER,
   });
 
-  this.flipVerticalRotationSwitch = new Gtk.Switch({
-    halign: Gtk.Align.END,
-    visible: true,
+  const setOffsetSpinButton = Gtk.SpinButton.new_with_range(-3, 3, 1);
+  setOffsetSpinButton.value = settings.get_int('orientation-offset');
+
+  const toggleLoggingSwitch = new Gtk.Switch({
+    active: settings.get_boolean('debug-logging'),
+    valign: Gtk.Align.CENTER
   });
 
-  this.flipOrientationSwitch = new Gtk.Switch({
-    halign: Gtk.Align.END,
-    visible: true,
-  });
+  settings.bind('invert-horizontal-rotation-direction',
+    invertHorizontalRotationSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
 
-  this.portraitDisplayFlipSwitch = new Gtk.Switch({
-    halign: Gtk.Align.END,
-    visible: true,
-  });
+  settings.bind('invert-vertical-rotation-direction',
+    invertVerticalRotationSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
 
-  prefsWidget.attach(this.flipHorizontalRotationSwitch, 1, 1, 1, 1);
-  prefsWidget.attach(this.flipVerticalRotationSwitch, 1, 2, 1, 1);
-  prefsWidget.attach(this.flipOrientationSwitch, 1, 3, 1, 1);
-  prefsWidget.attach(this.portraitDisplayFlipSwitch, 1, 4, 1, 1);
+  settings.bind('flip-orientation',
+    flipOrientationSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
 
-  // Bind the switch to the `flip-horizontal-rotation-direction` key
-  this.settings.bind('flip-horizontal-rotation-direction', this.flipHorizontalRotationSwitch,
-    'active', Gio.SettingsBindFlags.DEFAULT,
-  );
+  settings.bind('orientation-offset',
+    setOffsetSpinButton, 'value', Gio.SettingsBindFlags.DEFAULT);
 
-  // Bind the switch to the `flip-horizontal-rotation-direction` key
-  this.settings.bind('flip-vertical-rotation-direction', this.flipVerticalRotationSwitch,
-    'active', Gio.SettingsBindFlags.DEFAULT,
-  );
+  settings.bind('debug-logging',
+    toggleLoggingSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
 
-  // Bind the switch to the `flip-orientation` key
-  this.settings.bind('flip-orientation', this.flipOrientationSwitch,
-    'active', Gio.SettingsBindFlags.DEFAULT,
-  );
+  invertHorizontalRow.add_suffix(invertHorizontalRotationSwitch);
+  invertHorizontalRow.activatable_widget = invertHorizontalRotationSwitch;
 
-  this.settings.bind('portrait-display-flipped', this.portraitDisplayFlipSwitch, 
-    'active', Gio.SettingsBindFlags.DEFAULT,
-  );
+  invertVerticalRow.add_suffix(invertVerticalRotationSwitch);
+  invertVerticalRow.activatable_widget = invertVerticalRotationSwitch;
 
-  // Return our widget which will be added to the window
-  return prefsWidget;
+  flipOrientationRow.add_suffix(flipOrientationSwitch);
+  flipOrientationRow.activatable_widget = flipOrientationSwitch;
+
+  setOffsetRow.add_suffix(setOffsetSpinButton);
+  setOffsetRow.activatable_widget = setOffsetSpinButton;
+
+  toggleLoggingRow.add_suffix(toggleLoggingSwitch);
+  toggleLoggingRow.activatable_widget = toggleLoggingSwitch;
+
+  window._settings = settings;
+
+  // this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.screen-rotate');
+
+  //   // Create a parent widget that we'll return from this function
+  // const prefsWidget = new Gtk.Grid({
+  //   margin_top: 10,
+  //   margin_bottom: 10,
+  //   margin_start: 10,
+  //   margin_end: 10,
+  //   column_spacing: 12,
+  //   row_spacing: 12,
+  //   visible: true,
+  // });
+
+  // // Add a simple title and add it to the prefsWidget
+  // const title = new Gtk.Label({
+  //   label: `<b>${Me.metadata.name} Preferences</b>`,
+  //   halign: Gtk.Align.START,
+  //   use_markup: true,
+  //   visible: true,
+  // });
+  // prefsWidget.attach(title, 0, 0, 2, 1);
+
+  // // Create a label & switch for `flip-rotation-direction-horizontal`
+  // const flipRotationHorizontalLabel = new Gtk.Label({
+  //   label: 'Invert horizontal rotation:',
+  //   halign: Gtk.Align.START,
+  //   visible: true,
+  // });
+  // // Create a label & switch for `flip-rotation-direction-vertical`
+  // const flipRotationVerticalLabel = new Gtk.Label({
+  //   label: 'Invert vertical rotation:',
+  //   halign: Gtk.Align.START,
+  //   visible: true,
+  // });
+  // // Create a label & switch for `flip-orientation`
+  // const flipOrientationLabel = new Gtk.Label({
+  //   label: 'Flip orientation(default=Landscape):',
+  //   halign: Gtk.Align.START,
+  //   visible: true,
+  // });
+
+  // const portraitDisplayFlipLabel = new Gtk.Label({
+  //   label: 'Mirrored Portrait Display (ex: GPD Pocket 3):',
+  //   halign: Gtk.Align.START,
+  //   visible: true,
+  // });
+
+  // prefsWidget.attach(flipRotationHorizontalLabel, 0, 1, 1, 1);
+  // prefsWidget.attach(flipRotationVerticalLabel, 0, 2, 1, 1);
+  // prefsWidget.attach(flipOrientationLabel, 0, 3, 1, 1);
+  // prefsWidget.attach(portraitDisplayFlipLabel, 0, 4, 1, 1);
+
+  // this.flipHorizontalRotationSwitch = new Gtk.Switch({
+  //   halign: Gtk.Align.END,
+  //   visible: true,
+  // });
+
+  // this.flipVerticalRotationSwitch = new Gtk.Switch({
+  //   halign: Gtk.Align.END,
+  //   visible: true,
+  // });
+
+  // this.flipOrientationSwitch = new Gtk.Switch({
+  //   halign: Gtk.Align.END,
+  //   visible: true,
+  // });
+
+  // this.portraitDisplayFlipSwitch = new Gtk.Switch({
+  //   halign: Gtk.Align.END,
+  //   visible: true,
+  // });
+
+  // prefsWidget.attach(this.flipHorizontalRotationSwitch, 1, 1, 1, 1);
+  // prefsWidget.attach(this.flipVerticalRotationSwitch, 1, 2, 1, 1);
+  // prefsWidget.attach(this.flipOrientationSwitch, 1, 3, 1, 1);
+  // prefsWidget.attach(this.portraitDisplayFlipSwitch, 1, 4, 1, 1);
+
+  // // Bind the switch to the `flip-horizontal-rotation-direction` key
+  // this.settings.bind('flip-horizontal-rotation-direction', this.flipHorizontalRotationSwitch,
+  //   'active', Gio.SettingsBindFlags.DEFAULT,
+  // );
+
+  // // Bind the switch to the `flip-horizontal-rotation-direction` key
+  // this.settings.bind('flip-vertical-rotation-direction', this.flipVerticalRotationSwitch,
+  //   'active', Gio.SettingsBindFlags.DEFAULT,
+  // );
+
+  // // Bind the switch to the `flip-orientation` key
+  // this.settings.bind('flip-orientation', this.flipOrientationSwitch,
+  //   'active', Gio.SettingsBindFlags.DEFAULT,
+  // );
+
+  // this.settings.bind('portrait-display-flipped', this.portraitDisplayFlipSwitch, 
+  //   'active', Gio.SettingsBindFlags.DEFAULT,
+  // );
+
+  // // Return our widget which will be added to the window
+  // return prefsWidget;
 }
